47402fc29e2927d4e8ef6f8247905ba9c6d7b949
Вадим Малутин <malutin.vadim@yandex.ru>
2021-11-07 21:32:33 +0800

    Добавьте файлы проекта.

---
+++ Lab14/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+            cat.Showinfo();
+            dog.Showinfo();
+            Console.ReadKey();
+        }
+
+        abstract class Animal
+        {
+            public abstract string Name { get; set; }
+
+            public Animal()
+            {
+                Name = "Животное";
+            }
+            
+
+            public abstract void Say();
+            public void Showinfo()
+            {
+                Console.Write(Name);
+                Say();
+            }
+        }
+
+        class Cat : Animal
+        {
+            string name;
+
+            public override string Name
+            {   
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = "Кошка";
+                }
+            }
+
+            public override void Say()
+            {
+                Console.WriteLine(" Мяу");
+            }
+        }
+
+        class Dog : Animal
+        {
+            string name;
+
+            public override string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = "Собака";
+                }
+            }
+
+            public override void Say()
+            {
+                Console.WriteLine(" Гав");
+            }
+        }
+    }
+}
